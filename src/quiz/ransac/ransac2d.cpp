5a566555c4c9aecea16af71b29cdc047bfb228a1
Enno Faßbender <enno-fassbender@web.de>
2019-12-14 14:36:00 +0100

    final ransac 2D

--- src/quiz/ransac/ransac2d.cpp
+++ src/quiz/ransac/ransac2d.cpp
@@ -67,6 +67,7 @@ pcl::visualization::PCLVisualizer::Ptr initScene()
 
 std::unordered_set<int> Ransac(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, int maxIterations, float distanceTol)
 {
+
 	std::unordered_set<int> inliersResult, inliers_tmp;
 	//initialize the random seed
 	srand ( time(NULL) );
@@ -74,44 +75,60 @@ std::unordered_set<int> Ransac(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, int ma
 	int idx_p1, idx_p2;
 	float a,b,c,dis;
 
-	while(maxIterations--){
-		inliers_tmp.clear();
+	/*
+	 * Ensuring that the input cloud is big enough to find a model
+	 * */
+	if(cloud->points.size() > 2){
 
-		// Randomly sample subset and fit line
-		idx_p1 = rand() % cloud->points.size();
-		idx_p2 = rand() % cloud->points.size();
+		while(maxIterations--){
+			inliers_tmp.clear();
 
-		/*
-		 * Line formula
-		 * Ax + By + C = 0
-		 * for two given points:
-		 * (y1 -y2)x + (x2 -x1)y + (x1*y2 -x2*y1) = 0
-		 */
-		a = cloud->points[idx_p1].y - cloud->points[idx_p2].y;
-		b = cloud->points[idx_p2].x - cloud->points[idx_p1].x;
-		c = cloud->points[idx_p1].x * cloud->points[idx_p2].y - cloud->points[idx_p2].x * cloud->points[idx_p1].y;
-
-		// Measure distance between every point and fitted line
-		for(int i = 0; i < cloud->points.size(); i++){
 			/*
-			 * calculating distance for a point to the line model
-			 * Distance d = |Ax+By+C|/sqrt(A^2+B^2)
+			 * Picking randomly two points while making sure that a point didn't got choosen twice (items of set must be unique to be added)
 			 * */
-			dis = std::abs(a*cloud->points[i].x + b*cloud->points[i].y + c) / std::sqrt(std::pow(a,2) + std::pow(b,2));
-
-			// If distance is smaller than threshold count this index as inlier
-			if(dis <= distanceTol){
-				inliers_tmp.insert(i);
+			while(inliers_tmp.size() < 2){
+				inliers_tmp.insert(rand() % cloud->points.size());
 			}
-		}
-		/*
-		 * If this round has more inliers and therefore a better model, take it as best fit
-		 */
-		if(inliersResult.size() < inliers_tmp.size()){
-			inliersResult = inliers_tmp;
+			// Randomly sample subset and fit line
+			auto itr = inliers_tmp.begin();
+			idx_p1 = *itr;
+			*itr++;
+			idx_p2 = *itr;
+
+			/*
+			 * Line formula
+			 * Ax + By + C = 0
+			 * for two given points:
+			 * (y1 -y2)x + (x2 -x1)y + (x1*y2 -x2*y1) = 0
+			 */
+			a = cloud->points[idx_p1].y - cloud->points[idx_p2].y;
+			b = cloud->points[idx_p2].x - cloud->points[idx_p1].x;
+			c = cloud->points[idx_p1].x * cloud->points[idx_p2].y - cloud->points[idx_p2].x * cloud->points[idx_p1].y;
+
+			// Measure distance between every point and fitted line
+			for(int i = 0; i < cloud->points.size(); i++){
+				/*
+				 * calculating distance for a point to the line model
+				 * Distance d = |Ax+By+C|/sqrt(A^2+B^2)
+				 * */
+				dis = std::fabs(a*cloud->points[i].x + b*cloud->points[i].y + c) / std::sqrt(std::pow(a,2) + std::pow(b,2));
+
+				// If distance is smaller than threshold count this index as inlier
+				if(dis <= distanceTol){
+					inliers_tmp.insert(i);
+				}
+			}
+			/*
+			 * If this round has more inliers and therefore a better model, take it as best fit
+			 */
+			if(inliersResult.size() < inliers_tmp.size()){
+				inliersResult = inliers_tmp;
+			}
+
 		}
 
 	}
+
 	// Return indicies of inliers from fitted line with most inliers
 	return inliersResult;
 
@@ -128,7 +145,7 @@ int main ()
 	
 
 	// TODO: Change the max iteration and distance tolerance arguments for Ransac function
-	std::unordered_set<int> inliers = Ransac(cloud, 10, 1);
+	std::unordered_set<int> inliers = Ransac(cloud, 10, 1.0);
 
 	pcl::PointCloud<pcl::PointXYZ>::Ptr cloudInliers(new pcl::PointCloud<pcl::PointXYZ>());
 	pcl::PointCloud<pcl::PointXYZ>::Ptr cloudOutliers(new pcl::PointCloud<pcl::PointXYZ>());
